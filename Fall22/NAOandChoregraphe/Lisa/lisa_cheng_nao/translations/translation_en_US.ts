2daa34216f1ff87b11d7e4bec6619d7943b00833
chenglisa <97767296+chenglisa@users.noreply.github.com>
2022-12-15 14:25:44 -1000

    Add files via upload

---
+++ Fall22/NAOandChoregraphe/Lisa/lisa_cheng_nao/translations/translation_en_US.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello, I know we all been siting down. Lets all get up and do some stretches!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, I know we all been siting down. Lets all get up and do some stretches!</translation>
+        </message>
+        <message>
+            <source>Hello, I know we all been sitting down. Lets all get up and do some stretches!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, I know we all been sitting down. Lets all get up and do some stretches!</translation>
+        </message>
+        <message>
+            <source>Hello, I know we all been sitting down. Lets all get up and stretched!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, I know we all been sitting down. Lets all get up and stretched!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Goodbye!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Goodbye!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Lets all get up and do some stretches!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Lets all get up and do some stretches!</translation>
+        </message>
+        <message>
+            <source>Raise your hand up!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Raise your hand up!</translation>
+        </message>
+        <message>
+            <source>Raise your arm up!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Raise your arm up!</translation>
+        </message>
+        <message>
+            <source>Very good! Lets get some steps in! </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Very good! Lets get some steps in! </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Very good! Lets get some steps in! Take some steps forward.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Very good! Lets get some steps in! Take some steps forward.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Raise both arms up and hold it for a few seconds.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Raise both arms up and hold it for a few seconds.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Good, now take some steps back!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good, now take some steps back!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arms up/Say</name>
+        <message>
+            <source>Now, raise the other arm up!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Now, raise the other arm up!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Arms up/Say (1)</name>
+        <message>
+            <source>Raise one arm up!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Raise one arm up!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Each arm up/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Now, raise the other arm up!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Now, raise the other arm up!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Each arm up/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Raise one arm up!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Raise one arm up!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Now, raise the other arm up!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Now, raise the other arm up!</translation>
+        </message>
+        <message>
+            <source>Good, now lets take some steps back.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good, now lets take some steps back.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Raise one arm up!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Raise one arm up!</translation>
+        </message>
+    </context>
+</TS>
